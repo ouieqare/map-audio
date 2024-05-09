9c1cf4fff1d0104ced8fccc3829a27aefd55d737
Esther <84743798+estherberg@users.noreply.github.com>
2024-05-09 23:09:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ async function fetchAllAccounts() {
     }
 
     // return jsonResponse.data.filter(account => account.Layout && account.Layout.name === "Centre");
-return jsonResponse.data.filter(account => {
-      return account.Layout && account.Layout.name && account.Layout.name.toLowerCase() === "centre";
-    });
+// return jsonResponse.data.filter(account => {
+//       return account.Layout && account.Layout.name && account.Layout.name.toLowerCase() === "centre";
+//     });
+
+    return jsonResponse.data.filter(account => account.Layout);
+
   } catch (error) {
     console.error("Erreur lors de la récupération des comptes : ", error);
     return [];
